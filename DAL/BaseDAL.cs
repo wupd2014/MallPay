4233887bf417e5bc90cab7812ea8678e5165882b
wupd2014 <694828329@qq.com>
2017-10-27 10:38:22 +0800

    PetaPoco数持久化

---
+++ DAL/BaseDAL.cs
@@ -0,0 +1,20 @@
+﻿using PetaPoco;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class BaseDAL
+    {
+        protected Database db
+        {
+            get
+            {
+                return new Database("Schedules");
+            }
+        }
+    }
+}
