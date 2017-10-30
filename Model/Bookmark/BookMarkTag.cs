afcac1e7b20e2e7688325f52bf05929959a2968a
wupd2014 <694828329@qq.com>
2017-10-30 17:05:09 +0800

    新增书签管理实体类、书签便签及关系

---
+++ Model/Bookmark/BookMarkTag.cs
@@ -0,0 +1,11 @@
+﻿namespace Model.Bookmark
+{
+    /// <summary>
+    /// 书签标签
+    /// </summary>
+    public class BookMarkTag
+    {
+        public int Id { get; set; }
+        public string TagName { get; set; }
+    }
+}
---
+++ Model/Bookmark/Bookmark.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Model.Bookmark
+{
+    /// <summary>
+    /// 书签管理
+    /// </summary>
+    public class BookMark
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Url { get; set; }
+        public int pId { get; set; }
+        public DateTime CreateTime { get; set; }
+    }
+}
